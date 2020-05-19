8d508f786a2730787805453fa543aaef214e5727
Jerry Guerra <Jerry Guera>
2020-05-18 23:54:30 -0600

    ChangesFirstnameLastname

--- first.py
+++ first.py
@@ -5,4 +5,4 @@ if resp.status_code != 200:
     # This means something went wrong.
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 for todo_item in resp.json():
-    print('{} {}'.format(todo_item['id'], todo_item['summary']))
+    print('{} {}'.format(first_name, last_name))
